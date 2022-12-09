83d3df4d5c8a51ad80cd950db0d10b111f9b51de
HyoBeom <hyobeom@codemind.co.kr>
2022-12-10 04:41:45 +0900

    feat: add test

--- src/cursorMonitor/cursorMonitor.service.spec.ts
+++ src/cursorMonitor/cursorMonitor.service.spec.ts
@@ -2,17 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CursorMonitorService } from './cursorMonitor.service';
 
 describe('CursorMonitorService', () => {
-  let service: CursorMonitorService;
+  // let service: CursorMonitorService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CursorMonitorService],
-    }).compile();
+  // beforeEach(async () => {
+  //   const module: TestingModule = await Test.createTestingModule({
+  //     providers: [CursorMonitorService],
+  //   }).compile();
 
-    service = module.get<CursorMonitorService>(CursorMonitorService);
-  });
+  //   service = module.get<CursorMonitorService>(CursorMonitorService);
+  // });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  it('should be defined', () => {});
 });
