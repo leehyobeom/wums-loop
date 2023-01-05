fe2e64290b264f879c80ab606b2acb6e37b65e3e
HyoBeom <hyobeom@codemind.co.kr>
2023-01-05 12:54:08 +0900

    fix: docker push

--- dockerPush.sh
+++ dockerPush.sh
@@ -1,6 +1,6 @@
  #! /bin/bash
 for target in consumer driver gql producer
 do
-  docker build --build-arg TARGET_APP=$target -t $1/wums-$target:$2  ./ 
-  docker push $target/$wums-$target:$2
+   docker build --build-arg TARGET_APP=$target -t $1/wums-$target:$2  ./ 
+   docker push $1/wums-$target:$2
 done
