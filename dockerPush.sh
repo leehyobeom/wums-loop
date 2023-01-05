b233097f32c89f8a5ec2272e33e6d18a7c432173
HyoBeom <hyobeom@codemind.co.kr>
2023-01-05 14:03:05 +0900

    feat: revert test

--- dockerPush.sh
+++ dockerPush.sh
@@ -1,7 +1,6 @@
  #! /bin/bash
 for target in consumer driver gql producer
 do
-   # docker build --build-arg TARGET_APP=$target -t $1/wums-$target:$2  ./ 
-   # docker push $1/wums-$target:$2
-   echo target: $target 
+   docker build --build-arg TARGET_APP=$target -t $1/wums-$target:$2  ./ 
+   docker push $1/wums-$target:$2
 done
